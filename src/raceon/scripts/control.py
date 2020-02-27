ed9d1d03f08d25a1368a0331d68cc6eaa3e5d214
Marshall Demirjian <marshalljdem@gmail.com>
2020-02-26 17:59:30 -0800

    Update control.py

--- src/raceon/scripts/control.py
+++ src/raceon/scripts/control.py
@@ -39,7 +39,7 @@ class Controller():
     # TODO: Implement PID
     def pid(self, error):
         if error > 30:
-            return error * self.kp * 1.5
+            return error * self.kp * 5
         return error * self.kp
 
     def control_servo(self, error):
