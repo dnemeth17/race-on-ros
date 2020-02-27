1c6358c53a54dcbb23275cb9cf8a4eaea23ceddf
Marshall Demirjian <marshalljdem@gmail.com>
2020-02-26 17:29:43 -0800

    Update pos_estimation.py

--- src/raceon/scripts/pos_estimation.py
+++ src/raceon/scripts/pos_estimation.py
@@ -93,7 +93,7 @@ class PosEstimator():
     def pos_estimate(self, I):
         scan_line = self.scan_line;
         if self.previous_error:
-            scan_line -= int(abs(self.previous_error) / 25) * 50;
+            scan_line -= int((abs(self.previous_error) / 100) * 50);
         if scan_line < self.scan_line - 50:
             scan_line = self.scan_line - 50;
         # Select a horizontal line in the image
